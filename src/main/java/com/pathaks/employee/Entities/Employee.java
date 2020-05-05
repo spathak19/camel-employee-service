09a088209cb253defc0f9f9f5631f1f83a2d8872
spathak19 <sneharghya.pathak@gmail.com>
2020-05-05 15:50:20 +0530

    added jaxb annotations

--- src/main/java/com/pathaks/employee/Entities/Employee.java
+++ src/main/java/com/pathaks/employee/Entities/Employee.java
@@ -3,31 +3,43 @@ package com.pathaks.employee.Entities;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@XmlRootElement(name = "Employee")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Employee {
 
     @JsonProperty(value = "empid", required = false)
+    @XmlAttribute(name = "empid", required = false)
     private Integer empId;
 
-    
     @JsonProperty(value = "empfirstname", required = true)
+    @XmlAttribute(name = "empfirstname", required = true)
     private String empFirstName;
     
     @JsonProperty(value = "empmiddlename", required = false)
+    @XmlAttribute(name = "empmiddlename", required = false)
     private String empMiddleName;
     
     @JsonProperty(value = "emplastname", required = true)
+    @XmlAttribute(name = "emplastname", required = true)
     private String empLastName;
 
     @JsonProperty(value = "empsalary", required = true)
+    @XmlAttribute(name = "empsalary", required = true)
     private BigDecimal empSalary;
     
     @JsonProperty(value = "empdepartment", required = true)
+    @XmlAttribute(name = "empdepartment", required = true)
     private String empDepartment;
     
     @JsonProperty(value = "emphiredate", required = false)
+    @XmlAttribute(name = "emphiredate", required = false)
     private Date empHireDate;
     
     public Integer getEmpId() {
